7a770be369790ad331f09910f082d0108125fbc4
Abdul Majid <fa19-bse-014@cuilahore.edu.pk>
2022-06-15 13:03:25 +0500

    initial stage reade

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,10 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useSelector } from "react-redux";
+import {
+  BrowserRouter as Router,
+  Route,
+  Navigate,
+  Routes,
+} from "react-router-dom";
 import Cart from "./pages/Cart";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -6,21 +12,30 @@ import NotFound from "./pages/NotFound";
 import Product from "./pages/Product";
 import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
+import Success from "./pages/Success";
 
 function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route exact path="/" element={<Home />}></Route>
-                <Route path="/products" element={<ProductList />}></Route>
-                <Route path="/product/id" element={<Product />}></Route>
-                <Route path="/login" element={<Login />}></Route>
-                <Route path="/register" element={<Register />}></Route>
-                <Route path="/cart" element={<Cart />}></Route>
-                <Route path="*" element={<NotFound />}></Route>
-            </Routes>
-        </Router>
-    );
+  const user = useSelector((state) => state.user.currentUser);
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />}></Route>
+        <Route path="/products/:category" element={<ProductList />}></Route>
+        <Route path="/product/:id" element={<Product />}></Route>
+        <Route
+          path="/login"
+          element={user ? <Navigate to="/" replace /> : <Login />}
+        ></Route>
+        <Route
+          path="/register"
+          element={user ? <Navigate to="/" replace /> : <Register />}
+        ></Route>
+        <Route path="/cart" element={<Cart />}></Route>
+        <Route path="/success" element={<Success />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -1,86 +1,93 @@
 import styled from "@emotion/styled";
 import { Add, Remove } from "@mui/icons-material";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import Announcement from "../components/Announcement";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import { miniTab, mobile, tab } from "../responsive";
+import { useNavigate } from "react-router";
+import { userRequest } from "../requestMethods";
+import StripeCheckout from "react-stripe-checkout";
+
+const KEY = process.env.REACT_APP_STRIPE;
 
 const Container = styled.div``;
 
 const Wrapper = styled.div`
-    padding: 20px;
-    ${tab({ padding: "12px" })}
-    ${miniTab({ padding: "12px" })}
+  padding: 20px;
+  ${tab({ padding: "12px" })}
+  ${miniTab({ padding: "12px" })}
     ${mobile({ padding: "10px" })}
 `;
 
 const Title = styled.h1`
-    font-weight: 300;
-    text-align: center;
+  font-weight: 300;
+  text-align: center;
 `;
 
 const Top = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding: 20px;
-    ${tab({ padding: "10px" })}
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 20px;
+  ${tab({ padding: "10px" })}
 `;
 
 const TopButton = styled.button`
-    padding: 10px;
-    font-weight: 600;
-    cursor: pointer;
-    border: ${(props) => props.type === "filled" && "none"};
-    background-color: ${(props) =>
-        props.type === "filled" ? "black" : "transparent"};
-    color: ${(props) => props.type === "filled" && "white"};
+  padding: 10px;
+  font-weight: 600;
+  cursor: pointer;
+  border: ${(props) => props.type === "filled" && "none"};
+  background-color: ${(props) =>
+    props.type === "filled" ? "black" : "transparent"};
+  color: ${(props) => props.type === "filled" && "white"};
 `;
 
 const TopTexts = styled.div`
-    ${mobile({ display: "none" })}
-    ${miniTab({ display: "none" })}
+  ${mobile({ display: "none" })}
+  ${miniTab({ display: "none" })}
 `;
 const TopText = styled.span`
-    text-decoration: underline;
-    cursor: pointer;
-    margin: 0px 10px;
+  text-decoration: underline;
+  cursor: pointer;
+  margin: 0px 10px;
 `;
 
 const Bottom = styled.div`
-    display: flex;
-    justify-content: space-between;
-    ${mobile({ flexDirection: "column" })}
-    ${miniTab({ flexDirection: "column" })}
+  display: flex;
+  justify-content: space-between;
+  ${mobile({ flexDirection: "column" })}
+  ${miniTab({ flexDirection: "column" })}
 `;
 
 const Info = styled.div`
-    flex: 3;
+  flex: 3;
 `;
 
 const Product = styled.div`
-    display: flex;
-    justify-content: space-between;
-    ${mobile({ flexDirection: "column" })}
-    ${miniTab({ flexDirection: "column" })}
+  display: flex;
+  justify-content: space-between;
+  ${mobile({ flexDirection: "column" })}
+  ${miniTab({ flexDirection: "column" })}
 `;
 
 const ProductDetail = styled.div`
-    flex: 2;
-    display: flex;
+  flex: 2;
+  display: flex;
 `;
 
 const Image = styled.img`
-    width: 200px;
-    ${tab({ width: "100px" })}
+  width: 200px;
+  ${tab({ width: "100px" })}
 `;
 
 const Details = styled.div`
-    padding: 20px;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-    ${tab({ padding: "10px" })}
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  ${tab({ padding: "10px" })}
 `;
 
 const ProductName = styled.span``;
@@ -88,184 +95,195 @@ const ProductName = styled.span``;
 const ProductId = styled.span``;
 
 const ProductColor = styled.div`
-    width: 20px;
-    height: 20px;
-    border-radius: 50%;
-    background-color: ${(props) => props.color};
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
 `;
 
 const ProductSize = styled.span``;
 
 const PriceDetail = styled.div`
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
 
 const ProductAmountContainer = styled.div`
-    display: flex;
-    align-items: center;
-    margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
 `;
 
 const ProductAmount = styled.div`
-    font-size: 24px;
-    margin: 5px;
-    ${mobile({ margin: "5px 15px" })}
-    ${miniTab({ margin: "7px 17px" })}
+  font-size: 24px;
+  margin: 5px;
+  ${mobile({ margin: "5px 15px" })}
+  ${miniTab({ margin: "7px 17px" })}
     ${tab({ fontSize: "20px" })}
 `;
 
 const ProductPrice = styled.div`
-    font-size: 30px;
-    font-weight: 200;
-    ${miniTab({ marginBottom: "22px" })}
-    ${mobile({ marginBottom: "20px" })}
+  font-size: 30px;
+  font-weight: 200;
+  ${miniTab({ marginBottom: "22px" })}
+  ${mobile({ marginBottom: "20px" })}
     ${tab({ fontSize: "20px" })}
 `;
 
 const Hr = styled.hr`
-    background-color: #eee;
-    border: none;
-    height: 1px;
+  background-color: #eee;
+  border: none;
+  height: 1px;
 `;
 
 const Summary = styled.div`
-    flex: 1;
-    border: 0.5px solid lightgray;
-    border-radius: 10px;
-    padding: 20px;
-    height: 50vh;
+  flex: 1;
+  border: 0.5px solid lightgray;
+  border-radius: 10px;
+  padding: 20px;
+  height: 50vh;
 `;
 
 const SummaryTitle = styled.h1`
-    font-weight: 200;
-    ${tab({ fontSize: "1rem" })}
+  font-weight: 200;
+  ${tab({ fontSize: "1rem" })}
 `;
 
 const SummaryItem = styled.div`
-    margin: 30px 0px;
-    display: flex;
-    justify-content: space-between;
-    font-weight: ${(props) => props.type === "total" && "500"};
-    font-size: ${(props) => props.type === "total" && "24px"};
-    ${tab({ margin: "10px 0px" })}
+  margin: 30px 0px;
+  display: flex;
+  justify-content: space-between;
+  font-weight: ${(props) => props.type === "total" && "500"};
+  font-size: ${(props) => props.type === "total" && "24px"};
+  ${tab({ margin: "10px 0px" })}
 `;
 
 const SummaryItemText = styled.span`
-    ${tab({ fontSize: "12px" })}
+  ${tab({ fontSize: "12px" })}
 `;
 
 const SummaryItemPrice = styled.span`
-    ${tab({ fontSize: "12px" })}
+  ${tab({ fontSize: "12px" })}
 `;
 
 const Button = styled.button`
-    width: 100%;
-    padding: 10px;
-    background-color: black;
-    color: white;
-    font-weight: 600;
+  width: 100%;
+  padding: 10px;
+  background-color: black;
+  color: white;
+  font-weight: 600;
 `;
 
 const Cart = () => {
-    return (
-        <Container>
-            <Navbar />
-            <Announcement />
-            <Wrapper>
-                <Title>YOUR BAG</Title>
-                <Top>
-                    <TopButton>CONTINUE SHOPPING</TopButton>
-                    <TopTexts>
-                        <TopText>Shopping Bag(2)</TopText>
-                        <TopText>Your Wishlist (0)</TopText>
-                    </TopTexts>
-                    <TopButton type="filled">CHECKOUT NOW</TopButton>
-                </Top>
-                <Bottom>
-                    <Info>
-                        <Product>
-                            <ProductDetail>
-                                <Image src="https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1614188818-TD1MTHU_SHOE_ANGLE_GLOBAL_MENS_TREE_DASHERS_THUNDER_b01b1013-cd8d-48e7-bed9-52db26515dc4.png?crop=1xw:1.00xh;center,top&resize=480%3A%2A" />
-                                <Details>
-                                    <ProductName>
-                                        <b>Product:</b> JESSIE THUNDER SHOES
-                                    </ProductName>
-                                    <ProductId>
-                                        <b>ID:</b> 93813718293
-                                    </ProductId>
-                                    <ProductColor color="black" />
-                                    <ProductSize>
-                                        <b>Size:</b> 37.5
-                                    </ProductSize>
-                                </Details>
-                            </ProductDetail>
-                            <PriceDetail>
-                                <ProductAmountContainer>
-                                    <Add />
-                                    <ProductAmount>2</ProductAmount>
-                                    <Remove />
-                                </ProductAmountContainer>
-                                <ProductPrice>$ 30</ProductPrice>
-                            </PriceDetail>
-                        </Product>
-                        <Hr />
-                        <Product>
-                            <ProductDetail>
-                                <Image src="https://i.pinimg.com/originals/2d/af/f8/2daff8e0823e51dd752704a47d5b795c.png" />
-                                <Details>
-                                    <ProductName>
-                                        <b>Product:</b> HAKURA T-SHIRT
-                                    </ProductName>
-                                    <ProductId>
-                                        <b>ID:</b> 93813718293
-                                    </ProductId>
-                                    <ProductColor color="gray" />
-                                    <ProductSize>
-                                        <b>Size:</b> M
-                                    </ProductSize>
-                                </Details>
-                            </ProductDetail>
-                            <PriceDetail>
-                                <ProductAmountContainer>
-                                    <Add />
-                                    <ProductAmount>1</ProductAmount>
-                                    <Remove />
-                                </ProductAmountContainer>
-                                <ProductPrice>$ 20</ProductPrice>
-                            </PriceDetail>
-                        </Product>
-                    </Info>
-                    <Summary>
-                        <SummaryTitle>ORDER SUMMARY</SummaryTitle>
-                        <SummaryItem>
-                            <SummaryItemText>Subtotal</SummaryItemText>
-                            <SummaryItemPrice>$ 80</SummaryItemPrice>
-                        </SummaryItem>
-                        <SummaryItem>
-                            <SummaryItemText>
-                                Estimated Shipping
-                            </SummaryItemText>
-                            <SummaryItemPrice>$ 5.90</SummaryItemPrice>
-                        </SummaryItem>
-                        <SummaryItem>
-                            <SummaryItemText>Shipping Discount</SummaryItemText>
-                            <SummaryItemPrice>$ -5.90</SummaryItemPrice>
-                        </SummaryItem>
-                        <SummaryItem type="total">
-                            <SummaryItemText>Total</SummaryItemText>
-                            <SummaryItemPrice>$ 80</SummaryItemPrice>
-                        </SummaryItem>
-                        <Button>CHECKOUT NOW</Button>
-                    </Summary>
-                </Bottom>
-            </Wrapper>
-            <Footer />
-        </Container>
-    );
+  const cart = useSelector((state) => state.cart);
+  const [stripeToken, setStripeToken] = useState(null);
+  const history = useNavigate();
+
+  const onToken = (token) => {
+    setStripeToken(token);
+  };
+
+  useEffect(() => {
+    const makeRequest = async () => {
+      try {
+        const res = await userRequest.post("/checkout/payment", {
+          tokenId: stripeToken.id,
+          amount: 500,
+        });
+        history.push("/success", {
+          stripeData: res.data,
+          products: cart,
+        });
+      } catch {}
+    };
+    stripeToken && makeRequest();
+  }, [stripeToken, cart, history]);
+  return (
+    <Container>
+      <Navbar />
+      <Announcement />
+      <Wrapper>
+        <Title>YOUR BAG</Title>
+        <Top>
+          <TopButton>CONTINUE SHOPPING</TopButton>
+          <TopTexts>
+            <TopText>Shopping Bag(2)</TopText>
+            <TopText>Your Wishlist (0)</TopText>
+          </TopTexts>
+          <TopButton type="filled">CHECKOUT NOW</TopButton>
+        </Top>
+        <Bottom>
+          <Info>
+            {cart.products.map((product) => (
+              <Product>
+                <ProductDetail>
+                  <Image src={product.img} />
+                  <Details>
+                    <ProductName>
+                      <b>Product:</b> {product.title}
+                    </ProductName>
+                    <ProductId>
+                      <b>ID:</b> {product._id}
+                    </ProductId>
+                    <ProductColor color={product.color} />
+                    <ProductSize>
+                      <b>Size:</b> {product.size}
+                    </ProductSize>
+                  </Details>
+                </ProductDetail>
+                <PriceDetail>
+                  <ProductAmountContainer>
+                    <Add />
+                    <ProductAmount>{product.quantity}</ProductAmount>
+                    <Remove />
+                  </ProductAmountContainer>
+                  <ProductPrice>
+                    $ {product.price * product.quantity}
+                  </ProductPrice>
+                </PriceDetail>
+              </Product>
+            ))}
+            <Hr />
+          </Info>
+          <Summary>
+            <SummaryTitle>ORDER SUMMARY</SummaryTitle>
+            <SummaryItem>
+              <SummaryItemText>Subtotal</SummaryItemText>
+              <SummaryItemPrice>$ {cart.total}</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemText>Estimated Shipping</SummaryItemText>
+              <SummaryItemPrice>$ 5.90</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemText>Shipping Discount</SummaryItemText>
+              <SummaryItemPrice>$ -5.90</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem type="total">
+              <SummaryItemText>Total</SummaryItemText>
+              <SummaryItemPrice>$ {cart.total}</SummaryItemPrice>
+            </SummaryItem>
+            <StripeCheckout
+              name="Shopping Strom"
+              image="https://w7.pngwing.com/pngs/980/304/png-transparent-computer-icons-user-profile-avatar-heroes-silhouette-avatar.png"
+              billingAddress
+              shippingAddress
+              description={`Your total is $${cart.total}`}
+              amount={cart.total * 100}
+              token={onToken}
+              stripeKey={KEY}
+            >
+              <Button>CHECKOUT NOW</Button>
+            </StripeCheckout>
+          </Summary>
+        </Bottom>
+      </Wrapper>
+      <Footer />
+    </Container>
+  );
 };
 
 export default Cart;

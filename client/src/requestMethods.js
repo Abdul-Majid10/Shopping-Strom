03fc2f3afa105b3e94cb6b0034fb675fdf4f4e05
Abdul Majid <74697541+Abdul-Majid10@users.noreply.github.com>
2023-03-13 13:39:18 +0500

    Token is undefined Error - Change header to header because req have headers object instead of header.

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -13,5 +13,5 @@ export const publicRequest = axios.create({
 
 export const userRequest = axios.create({
   baseURL: BASE_URL,
-  header: { token: `Bearer ${TOKEN}` },
+  headers: { token: `Bearer ${TOKEN}` },
 });
